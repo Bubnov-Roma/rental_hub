bc63428a4691e8772532077bc98cd92a71b63336
Kilkun <kilkun@icloud.com>
2025-12-22 17:36:43 +0400

    refactor: update root and (dashboard) layouts

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -7,23 +7,19 @@ export default async function DashboardLayout({
 }: {
 	children: React.ReactNode;
 }) {
-	// 1. Инициализируем клиент Supabase на сервере
 	const supabase = await createClient();
-
-	// 2. Получаем пользователя.
 	const {
 		data: { user },
 		error,
 	} = await supabase.auth.getUser();
 
-	// 3. Если пользователя нет или произошла ошибка — мгновенный редирект
 	if (error || !user) {
 		redirect("/auth/login");
 	}
 
 	return (
 		<div className="flex min-h-screen">
-			<Sidebar user={user} />
+			<Sidebar />
 			<div className="flex-1">
 				<div className="container mx-auto p-6">{children}</div>
 			</div>
