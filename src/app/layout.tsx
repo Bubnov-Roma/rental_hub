639fb3a72c754349be1c69ac16ef9570f7171a50
Kilkun <kilkun@icloud.com>
2025-12-22 17:36:43 +0400

    refactor: update root and (dashboard) layouts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import { Header } from "@/components/layouts/Header";
 import { Toaster } from "@/components/ui/sonner";
 import { createClient } from "@/lib/supabase/server";
 import "./globals.css";
+import { AuthProvider } from "@/providers/auth-provider";
+import { UnsavedChangesGuard } from "@/providers/unsaved-changes-guard";
 
 const inter = Inter({ subsets: ["latin", "cyrillic"] });
 
@@ -22,15 +24,24 @@ export default async function RootLayout({
 	const {
 		data: { user },
 	} = await supabase.auth.getUser();
+
 	return (
 		<html lang="ru">
 			<body className={inter.className}>
-				<div className="flex min-h-screen flex-col">
-					<Header user={user} />
-					<main className="flex-1">{children}</main>
-					<Footer />
-					<Toaster position="top-right" expand={false} richColors closeButton />
-				</div>
+				<AuthProvider initialUser={user}>
+					<UnsavedChangesGuard />
+					<div className="flex min-h-screen flex-col">
+						<Header />
+						<main className="flex-1">{children}</main>
+						<Footer />
+						<Toaster
+							position="top-right"
+							expand={false}
+							richColors
+							closeButton
+						/>
+					</div>
+				</AuthProvider>
 			</body>
 		</html>
 	);
