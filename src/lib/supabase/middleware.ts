ff9e5be655f80d013649c7e423ccbab602c54257
Kilkun <kilkun@icloud.com>
2026-01-16 15:50:22 +0400

    refactor: update middleware, add some new roles

--- src/lib/supabase/middleware.ts
+++ src/lib/supabase/middleware.ts
@@ -2,9 +2,8 @@ import { createServerClient } from "@supabase/ssr";
 import { type NextRequest, NextResponse } from "next/server";
 
 export async function updateSession(request: NextRequest) {
-	let supabaseResponse = NextResponse.next({
-		request,
-	});
+	let supabaseResponse = NextResponse.next({ request });
+
 	const supabase = createServerClient(
 		process.env.NEXT_PUBLIC_SUPABASE_URL || "",
 		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "",
@@ -17,9 +16,7 @@ export async function updateSession(request: NextRequest) {
 					cookiesToSet.forEach(({ name, value }) => {
 						request.cookies.set(name, value);
 					});
-					supabaseResponse = NextResponse.next({
-						request,
-					});
+					supabaseResponse = NextResponse.next({ request });
 					cookiesToSet.forEach(({ name, value, options }) => {
 						supabaseResponse.cookies.set(name, value, options);
 					});
@@ -27,8 +24,47 @@ export async function updateSession(request: NextRequest) {
 			},
 		}
 	);
+
 	const {
 		data: { user },
 	} = await supabase.auth.getUser();
+	const url = request.nextUrl.clone();
+	const { pathname } = url;
+
+	// Paths
+	const isAuthRoute = pathname.startsWith("/auth");
+	const isAdminRoute = pathname.startsWith("/admin");
+	const isProtectedRoute =
+		pathname.startsWith("/dashboard") || pathname.startsWith("/booking");
+
+	// 1. Redirect unauthorized
+	if (!user && (isProtectedRoute || isAdminRoute)) {
+		url.pathname = "/auth/login";
+		url.searchParams.set("redirect", pathname);
+		return { supabaseResponse: NextResponse.redirect(url), user: null };
+	}
+
+	// 2.Checking ROLES in the database for the admin panel
+	if (user && isAdminRoute) {
+		const { data: profile } = await supabase
+			.from("profiles")
+			.select("role")
+			.eq("id", user.id)
+			.single();
+
+		const role = profile?.role;
+
+		if (role !== "admin" && role !== "manager") {
+			url.pathname = "/dashboard";
+			return { supabaseResponse: NextResponse.redirect(url), user };
+		}
+	}
+
+	// 3. Redirect authorized users from login pages
+	if (user && isAuthRoute) {
+		url.pathname = "/dashboard";
+		return { supabaseResponse: NextResponse.redirect(url), user };
+	}
+
 	return { supabase, user, supabaseResponse };
 }
