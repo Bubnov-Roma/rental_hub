35bca6ce5c9ab7562fc3d84052b5f6d300077602
Kilkun <kilkun@icloud.com>
2026-01-19 15:06:44 +0400

    feat: add constants

---
+++ src/constants/index.ts
@@ -0,0 +1,6 @@
+export * from "./brands";
+export * from "./experience";
+export * from "./form-steps.config";
+export * from "./referral";
+export * from "./relations";
+export * from "./socials";
---
+++ src/constants/referral.ts
@@ -0,0 +1,8 @@
+export const REFERRAL_OPTIONS = [
+	{ id: "website", label: "Сайт", color: "#34d399" },
+	{ id: "vk", label: "ВКонтакте", color: "#4c75a3" },
+	{ id: "search_engine", label: "Поисковик", color: "#60a5fa" },
+	{ id: "friends", label: "Рекомендация", color: "#f472b6" },
+	{ id: "photo_school", label: "Фотошкола", color: "#fbbf24" },
+	{ id: "other", label: "Другое", color: "#a78bfa" },
+];
