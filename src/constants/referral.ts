ee7efb196dca868bba2ea202979f314c6dfb036d
Kilkun <kilkun@icloud.com>
2026-01-25 12:59:41 +0400

    refactor: update constants

--- src/constants/referral.ts
+++ src/constants/referral.ts
@@ -1,8 +1,28 @@
 export const REFERRAL_OPTIONS = [
-	{ id: "website", label: "Сайт", color: "#34d399" },
-	{ id: "vk", label: "ВКонтакте", color: "#4c75a3" },
-	{ id: "search_engine", label: "Поисковик", color: "#60a5fa" },
-	{ id: "friends", label: "Рекомендация", color: "#f472b6" },
-	{ id: "photo_school", label: "Фотошкола", color: "#fbbf24" },
-	{ id: "other", label: "Другое", color: "#a78bfa" },
+	{ id: "website", label: "Сайт", color: "#34d399", placeholder: "" },
+	{ id: "vk", label: "ВКонтакте", color: "#4c75a3", placeholder: "" },
+	{
+		id: "search_engine",
+		label: "Поисковик",
+		color: "#60a5fa",
+		placeholder: "",
+	},
+	{
+		id: "friends",
+		label: "Рекомендация",
+		color: "#f472b6",
+		placeholder: "Подскажите кто нас  порекомендовал",
+	},
+	{
+		id: "photo_school",
+		label: "Фотошкола",
+		color: "#fbbf24",
+		placeholder: "Расскажите в какой фотошколе Вы узнали про нас",
+	},
+	{
+		id: "other",
+		label: "Другое",
+		color: "#a78bfa",
+		placeholder: "Поделитесь историей как Вы нас нашли",
+	},
 ];
