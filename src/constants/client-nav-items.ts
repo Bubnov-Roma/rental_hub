8cd5250467513abc9ec283c49ba9b603b62c28c2
Kilkun <kilkun@icloud.com>
2026-01-29 16:00:16 +0400

    refactor: update some constants and types

---
+++ src/constants/client-nav-items.ts
@@ -0,0 +1,22 @@
+import {
+	Bell,
+	Calendar,
+	LayoutDashboard,
+	Package,
+	Settings,
+	UserIcon,
+} from "lucide-react";
+
+export const CLIENT_NAV_ITEMS = [
+	{ name: "Дашборд", href: "/dashboard", icon: LayoutDashboard },
+	{ name: "Бронирования", href: "/dashboard/bookings", icon: Package },
+	{ name: "Календарь", href: "/dashboard/calendar", icon: Calendar },
+	{
+		name: "Уведомления",
+		href: "/dashboard/notifications",
+		icon: Bell,
+		badge: 3,
+	},
+	{ name: "Профиль", href: "/dashboard/profile", icon: UserIcon },
+	{ name: "Настройки", href: "/dashboard/settings", icon: Settings },
+];
