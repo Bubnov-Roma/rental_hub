c7e172b8c46d020f98f751c6da595885508852dc
Kilkun <kilkun@icloud.com>
2026-01-30 11:06:48 +0400

    feat: update radix ui components

--- src/components/ui/animate-spin.tsx
+++
@@ -1,3 +0,0 @@
-export const AnimateSpin = () => (
-	<div className="size-5 animate-spin rounded-full border-2 border-primary border-t-transparent mx-auto" />
-);
--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -19,11 +19,9 @@ function Card({ className, ...props }: React.ComponentProps<"div">) {
 					backgroundImage: `url("data:image/svg+xml,%3Csvg viewBox='0 0 200 200' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.65' numOctaves='3' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)'/%3E%3C/svg%3E")`,
 				}}
 			/>
-
 			{/* 2. Hover Glow & Refraction */}
 			<div className="absolute top-0 left-0 right-0 inset-0 bg-linear-to-br from-white/10 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-500 pointer-events-none z-0" />
 			<div className="absolute top-0 left-0 right-0 h-px bg-linear-to-r from-transparent via-white/20 to-transparent pointer-events-none z-0" />
-
 			{/* 3. Контентная часть (z-10, чтобы быть выше шума) */}
 			<div className="relative z-10 flex flex-col h-full">{props.children}</div>
 		</div>
@@ -57,7 +55,7 @@ function CardDescription({ className, ...props }: React.ComponentProps<"div">) {
 	return (
 		<div
 			data-slot="card-description"
-			className={cn("text-sm text-white/50", className)}
+			className={cn("text-sm text-foreground/50", className)}
 			{...props}
 		/>
 	);
